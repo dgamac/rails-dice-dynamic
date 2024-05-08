dfaf50b064c860933f7e8330c202874c58b4e8b3
dgamac <165616196+dgamac@users.noreply.github.com>
2024-05-08 02:55:52 +0000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get("/", {:controller => "home", action:"home"})
 
+  get("/dice/:number_of_dice/:how_many_sides", {:controller => "home", :action => "dice"})
 end
